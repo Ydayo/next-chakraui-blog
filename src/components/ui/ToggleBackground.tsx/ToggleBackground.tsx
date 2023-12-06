020258265aec38e7653876f076bc8e3b32222d10
IshikawaTokyo <yyusb.mac@gmail.com>
2023-12-06 17:34:21 +0900

    change: typeを変更

--- src/components/ui/ToggleBackground.tsx/ToggleBackground.tsx
+++ src/components/ui/ToggleBackground.tsx/ToggleBackground.tsx
@@ -1,12 +1,12 @@
 "use client";
 import { useColorChange } from "@/hooks/ToggleColorChage";
 import { Box } from "@chakra-ui/react";
-import { FC, ReactNode } from "react";
+import { FC, LegacyRef, ReactNode } from "react";
 
 const ToggleBackground: FC<{
   children: ReactNode;
   className?: string;
-  myRef?: any;
+  myRef?: LegacyRef<HTMLDivElement> | undefined;
 }> = ({ children, className, myRef }) => {
   const { fromBackground } = useColorChange();
   return (
