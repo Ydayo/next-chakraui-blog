f3c0d8bccbc04c3b6d2e82719a20345b62d7d9ef
IshikawaTokyo <yyusb.mac@gmail.com>
2023-12-07 11:55:27 +0900

    add: toggleMenuの実装

--- src/components/ui/Header/Header.tsx
+++ src/components/ui/Header/Header.tsx
@@ -6,15 +6,20 @@ import { Sun } from "@/components/icons/Sun";
 import { Moon } from "@/components/icons/Moon";
 import { useColorChange } from "@/hooks/ToggleColorChage";
 import { Button } from "@chakra-ui/react";
-import { useScroll } from "@/hooks/Scroll";
+import { Menu } from "@/components/icons/Menu";
+import { Close } from "@/components/icons/Close";
 
 const Header: FC = () => {
   const [toggle, setToggle] = useState(false);
+  const [menuToggle, setMenuToggle] = useState(false);
   const { toggleColorMode, fromBackground } = useColorChange();
   const handleToggle = (e: MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
     toggleColorMode();
     setToggle(!toggle);
+  };
+  const handleMenuToggle = () => {
+    setMenuToggle(!menuToggle);
   };
   return (
     <header className="w-full h-[80px] flex justify-center text-center items-center border-b border-white">
@@ -35,6 +40,27 @@ const Header: FC = () => {
         >
           {toggle ? <Moon /> : <Sun />}
         </Button>
+      </div>
+      <Button
+        className=" rounded-full hover:bg-white-1/2 w-12 h-12 absolute t-4 right-[80px]"
+        onClick={handleMenuToggle}
+      >
+        <Menu color="white" className=" w-full h-full" />
+      </Button>
+      <div
+        className={`w-[240px] h-full absolute top-0 right-[-100%] z-[1000] bg-black transition-all ${
+          menuToggle && "right-0"
+        }`}
+      >
+        <Button onClick={handleMenuToggle} className="absolute top-0 right-0">
+          <Close color="white" />
+        </Button>
+        <Link className="text-white mt-14 block font-bold" href={"/"}>
+          Homeへ戻る
+        </Link>
+        <Link className="text-white mt-5 block font-bold" href={"/blog"}>
+          Blogへ行く
+        </Link>
       </div>
     </header>
   );
